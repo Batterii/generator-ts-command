9cf59c22eb0f90ee83b8e0a5f983347f32496693
Steve Ripberger <steve.ripberger@gmail.com>
2020-06-18 21:12:22 -0400

    Update linter config and fix errors

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,27 +1,27 @@
-const { Generator } = require('@batterii/yeoman-helpers');
-const { isString } = require('lodash');
-const { validateCommandName } = require('@batterii/yeoman-validators');
+const {Generator} = require("@batterii/yeoman-helpers");
+const {isString} = require("lodash");
+const {validateCommandName} = require("@batterii/yeoman-validators");
 
 class CommandGenerator extends Generator {
 	constructor(args, opts) {
 		super(args, opts);
 
 		this.optionPrompt({
-			type: 'input',
-			name: 'name',
-			alias: 'n',
-			description: 'Name of the commmand',
-			message: 'Enter the command name.',
+			type: "input",
+			name: "name",
+			alias: "n",
+			description: "Name of the commmand",
+			message: "Enter the command name.",
 			validate: validateCommandName,
 		});
 	}
 
 	addCommand() {
 		// Get the command name from options.
-		const { name } = this.options;
+		const {name} = this.options;
 
 		// Copy the new command source file to the src/bin directory.
-		this.copyTemplate('command.ts', `src/bin/${name}.ts`);
+		this.copyTemplate("command.ts", `src/bin/${name}.ts`);
 
 		/*
 		 * Create a customizer function to prevent overwriting of
@@ -35,7 +35,7 @@ class CommandGenerator extends Generator {
 
 		// Add bin entry for the new command in package.json.
 		this.extendPackage({
-			bin: { [name]: `dist/bin/${name}.js` },
+			bin: {[name]: `dist/bin/${name}.js`},
 		}, overwriteCheck);
 
 		// Add a start script for the new command.
